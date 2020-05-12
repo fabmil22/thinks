38089782e2337bd1951821aa1422a624ef387b00
fabian <fabmil22@gmail.com>
2020-05-12 19:38:27 +0200

    cambio en el servicio company

--- src/app/company/company.service.spec.ts
+++ src/app/company/company.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
+import {HttpClientModule} from '@angular/common/http';
 import { CompanyService } from './company.service';
 
 describe('CompanyService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let service: CompanyService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports:[ HttpClientModule],
+      providers:[CompanyService]
+    });
+
+    service = TestBed.get(CompanyService);
+});
 
   it('should be created', () => {
-    const service: CompanyService = TestBed.get(CompanyService);
+
     expect(service).toBeTruthy();
   });
 });
