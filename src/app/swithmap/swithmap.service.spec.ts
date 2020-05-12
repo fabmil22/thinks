509c12bdf7020a5dcf11cea21fc103e024a760ba
fabian <fabmil22@gmail.com>
2020-05-12 19:24:16 +0200

    cambio en el servicio swithmap

--- src/app/swithmap/swithmap.service.spec.ts
+++ src/app/swithmap/swithmap.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-
+import {HttpClientModule} from '@angular/common/http';
 import { SwithmapService } from './swithmap.service';
 
+
 describe('SwithmapService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: SwithmapService;
+
+  beforeEach(() =>{ TestBed.configureTestingModule({
+    imports: [ HttpClientModule],
+    providers: [SwithmapService]
+  });
+  service = TestBed.get(SwithmapService);}
+  ) ;
+
 
   it('should be created', () => {
-    const service: SwithmapService = TestBed.get(SwithmapService);
-    expect(service).toBeTruthy();
+     expect(service).toBeTruthy();
   });
+
+
 });
